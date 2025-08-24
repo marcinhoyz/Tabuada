d704e9d49b3b22b5a646ac8795a2a8cd9249b627
marcinhoyz <marciodiasfilho4@gmail.com>
2025-08-24 18:23:19 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero ;
+            Console.WriteLine("Informe o número que deseja ver a tabuda: ");
+            numero = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+
+            }
+        }
+    }
+}
+
+
